5a426f657be6353667e9356aa1fe01fe42c30ae0
federico tropiane <fedetrop23@gmail.com>
2025-10-14 00:05:25 -0300

    Modificamos archivo env.ts para asignar variables locales

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -11,7 +11,7 @@ for(const key of requiereVariables){
 };
 
 export const ENV = {
-    PORT: process.env.PORT,
-    MONGODB: process.env.MONGODB,
-    JWT_SECRET: process.env.JWT_SECRET
+    PORT: process.env.PORT || 3000,
+    MONGODB: process.env.MONGODB || "localhost:27017/base",
+    JWT_SECRET: process.env.JWT_SECRET || "ClaveSecreta"
 };
