bd4579406ad774069843ea868bdbfe35d32ba8c4
federico tropiane <fedetrop23@gmail.com>
2025-10-25 00:22:39 -0300

    modificamos cors para frontend local

--- backend/index.ts
+++ backend/index.ts
@@ -13,12 +13,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET || 'cookie_secret_key'))
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(
-    {
-        origin: "*",
-        credentials: true
-    }
-));
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 const connection = new Mongo();
 
 app.use('/api/form', formRouter);
