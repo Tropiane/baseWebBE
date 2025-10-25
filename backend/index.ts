e34806a1433517da48b0ad1fdad16b8403909513
federico tropiane <fedetrop23@gmail.com>
2025-10-24 21:31:10 -0300

    agregamos variable de entorno para el uso de CORS

--- backend/index.ts
+++ backend/index.ts
@@ -14,7 +14,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET || 'cookie_secret_key'))
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: process.env.CORS_ORIGIN || "http://localhost:5173",
         credentials: true
     }
 ));
