3c3cd4ae2596e521240a6bff8524068afc75089e
federico tropiane <fedetrop23@gmail.com>
2025-10-29 12:34:56 -0300

    Cambiamos error con middlewares

--- backend/src/modules/form/form.router.ts
+++ backend/src/modules/form/form.router.ts
@@ -8,8 +8,8 @@ const controller = new formController();
 
 router.get("/", validateAdmin, (req: Request, res: Response) => {controller.getForms(req, res)});
 router.post("/", async (req: Request, res: Response)=>{controller.createForm(req, res);});
-router.patch("/", validateAdmin, async (req:Request<{},{}, {id: number, comment: string}>, res:Response)=>{await controller.updateForm(req, res)  });
-router.delete('/', validateAdmin, async (req: Request<{},{},{id: number}>, res:Response)=>{ await controller.deleteForm(req, res)})
-router.patch("/change-status", validateAdmin, async (req:Request<{},{},{formId:number, status:string}>, res:Response)=>{await controller.changeFormStatus(req,res)})
+router.patch("/", async (req:Request<{},{}, {id: number, comment: string}>, res:Response)=>{await controller.updateForm(req, res)  });
+router.delete('/', async (req: Request<{},{},{id: number}>, res:Response)=>{ await controller.deleteForm(req, res)})
+router.patch("/change-status", async (req:Request<{},{},{formId:number, status:string}>, res:Response)=>{await controller.changeFormStatus(req,res)})
 const formRouter = router;
 export default formRouter;
