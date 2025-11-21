dc4497ec913e4e391c4cd9e777cb6a0e317a5dbf
federico tropiane <fedetrop23@gmail.com>
2025-11-21 00:26:33 -0300

    modificamos middleware para capturar errores 3

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -15,15 +15,15 @@ export default class Server {
     }
 
     private middlewares() {
-        this.app.use(express.json());
-        this.app.use(cookieParser(ENV.COOKIE_SECRET || 'cookie_secret_key'));
-
         this.app.use(cors({
             origin: [ENV.CORS_ORIGIN, "http://localhost:5173"],
             credentials: true,
             methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             allowedHeaders: ["Content-Type", "Authorization"]
         }));
+        this.app.use(express.json());
+        this.app.use(cookieParser(ENV.COOKIE_SECRET || 'cookie_secret_key'));
+
 
         this.app.use(express.urlencoded({ extended: true }));
     }
